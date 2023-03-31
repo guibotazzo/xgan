69943884268d5c8bec6f199d4478d7445e72fb09
guilherme <guilherme.botazzo@unesp.br>
2023-03-31 16:21:43 +0200

    added celeba training

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,18 +1,73 @@
-from torchvision import transforms
+import os
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize, CenterCrop
 from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, FashionMNIST, ImageFolder
+from gdown import download
+from zipfile import ZipFile
 
 
-def make_mnist_dataset():
+def make_mnist_dataset(batch_size: int):
     # loading the dataset
-    dataset = MNIST(root='./datasets', download=True,
-                    transform=transforms.Compose([
-                        transforms.Resize(28),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.5,), (0.5,)),
+    dataset = MNIST(root='./datasets',
+                    download=True,
+                    transform=Compose([
+                        Resize(28),
+                        ToTensor(),
+                        Normalize((0.5,), (0.5,)),
                     ]))
 
-    dataloader = DataLoader(dataset, batch_size=64,
-                            shuffle=True, num_workers=2)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
 
     return dataloader
+
+
+def make_fmnist_dataset(batch_size: int):
+    # loading the dataset
+    dataset = FashionMNIST(root='./datasets',
+                           download=True,
+                           transform=Compose([
+                               Resize(28),
+                               ToTensor(),
+                               Normalize((0.5,), (0.5,)),
+                           ]))
+
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    return dataloader
+
+
+def make_celebA_dataset(image_size: int, batch_size: int):
+    path = './datasets/CelebA/data.zip'
+
+    if not os.path.exists(path):
+        url = 'https://drive.google.com/uc?id=1O7m1010EJjLE5QxLZiM9Fpjs7Oj6e684'
+
+        download(url, path, quiet=True)
+
+        with ZipFile(path, 'r') as zipobj:
+            zipobj.extractall('./datasets/CelebA/')
+
+    dataset = ImageFolder(root='./datasets/CelebA/',
+                          transform=Compose([
+                              Resize(image_size),
+                              CenterCrop(image_size),
+                              ToTensor(),
+                              Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                          ]))
+
+    dataloader = DataLoader(dataset,
+                            batch_size=batch_size,
+                            shuffle=True,
+                            num_workers=2
+                            )
+
+    return dataloader
+
+
+def make_dataset(dataset: str, batch_size: int, image_size: int):
+    if dataset == 'mnist':
+        return make_mnist_dataset(batch_size)
+    elif dataset == 'fminist':
+        return make_fmnist_dataset(batch_size)
+    else:
+        return make_celebA_dataset(image_size, batch_size)
--- utils/models.py
+++ utils/models.py
@@ -67,3 +67,70 @@ class DiscriminatorMNIST(nn.Module):
         output = self.network(input)
         return output.view(-1, 1).squeeze(1)
 
+
+class GeneratorCelebA(nn.Module):
+    def __init__(self):
+        super(GeneratorCelebA, self).__init__()
+        self.nz = 100  # Size of z latent vector
+        self.ngf = 64  # Size of feature maps in generator
+        self.nc = 3  # Number of channels in the training images
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(in_channels=self.nz,
+                               out_channels=self.ngf * 8,
+                               kernel_size=4,
+                               stride=1,
+                               padding=0,
+                               bias=False),
+            nn.BatchNorm2d(self.ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(self.ngf * 8, self.ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(self.ngf * 2, self.ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(self.ngf, self.nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class DiscriminatorCelebA(nn.Module):
+    def __init__(self):
+        super(DiscriminatorCelebA, self).__init__()
+        self.ndf = 64  # Size of feature maps in discriminator
+        self.nc = 3  # Number of channels in the training images
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(self.nc, self.ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(self.ndf, self.ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(self.ndf * 2, self.ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(self.ndf * 4, self.ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(self.ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
