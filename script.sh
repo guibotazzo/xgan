e77eef3720e06f7932477a26a48e2b14e6faf618
guilherme <guilherme.botazzo@unesp.br>
2023-11-09 19:30:03 +0100

    lil adjustment

--- script.sh
+++ script.sh
@@ -1,126 +1,124 @@
 #!/bin/bash
 
-cd Documentos/botazzo/xgan/
-
 rm -r weights
 rm -r runs
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run1/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run1/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run1/
-mv runs/* weights/xdcgan/gradcam/mnist/run1/
+mkdir weights/xdcgan/mnist/gradcam/run1/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run1/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run1/
+mv runs/* weights/xdcgan/mnist/gradcam/run1/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run2/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run2/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run2/
-mv runs/* weights/xdcgan/gradcam/mnist/run2/
+mkdir weights/xdcgan/mnist/gradcam/run2/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run2/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run2/
+mv runs/* weights/xdcgan/mnist/gradcam/run2/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run3/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run3/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run3/
-mv runs/* weights/xdcgan/gradcam/mnist/run3/
+mkdir weights/xdcgan/mnist/gradcam/run3/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run3/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run3/
+mv runs/* weights/xdcgan/mnist/gradcam/run3/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run4/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run4/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run4/
-mv runs/* weights/xdcgan/gradcam/mnist/run4/
+mkdir weights/xdcgan/mnist/gradcam/run4/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run4/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run4/
+mv runs/* weights/xdcgan/mnist/gradcam/run4/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run5/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run5/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run5/
-mv runs/* weights/xdcgan/gradcam/mnist/run5/
+mkdir weights/xdcgan/mnist/gradcam/run5/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run5/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run5/
+mv runs/* weights/xdcgan/mnist/gradcam/run5/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run6/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run6/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run6/
-mv runs/* weights/xdcgan/gradcam/mnist/run6/
+mkdir weights/xdcgan/mnist/gradcam/run6/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run6/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run6/
+mv runs/* weights/xdcgan/mnist/gradcam/run6/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run7/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run7/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run7/
-mv runs/* weights/xdcgan/gradcam/mnist/run7/
+mkdir weights/xdcgan/mnist/gradcam/run7/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run7/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run7/
+mv runs/* weights/xdcgan/mnist/gradcam/run7/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run8/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run8/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run8/
-mv runs/* weights/xdcgan/gradcam/mnist/run8/
+mkdir weights/xdcgan/mnist/gradcam/run8/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run8/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run8/
+mv runs/* weights/xdcgan/mnist/gradcam/run8/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run9/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run9/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run9/
-mv runs/* weights/xdcgan/gradcam/mnist/run9/
+mkdir weights/xdcgan/mnist/gradcam/run9/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run9/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run9/
+mv runs/* weights/xdcgan/mnist/gradcam/run9/
 
 python xdcgan.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xdcgan/gradcam/mnist/run10/
-mv weights/xdcgan/gradcam/mnist/disc_epoch_100.pth weights/xdcgan/gradcam/mnist/run10/
-mv weights/xdcgan/gradcam/mnist/gen_epoch_100.pth weights/xdcgan/gradcam/mnist/run10/
-mv runs/* weights/xdcgan/gradcam/mnist/run10/
+mkdir weights/xdcgan/mnist/gradcam/run10/
+mv weights/xdcgan/mnist/gradcam/disc_epoch_100.pth weights/xdcgan/mnist/gradcam/run10/
+mv weights/xdcgan/mnist/gradcam/gen_epoch_100.pth weights/xdcgan/mnist/gradcam/run10/
+mv runs/* weights/xdcgan/mnist/gradcam/run10/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run1/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run1/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run1/
-mv runs/* weights/xwgangp/gradcam/mnist/run1/
+mkdir weights/xwgangp/mnist/gradcam/run1/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run1/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run1/
+mv runs/* weights/xwgangp/mnist/gradcam/run1/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run2/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run2/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run2/
-mv runs/* weights/xwgangp/gradcam/mnist/run2/
+mkdir weights/xwgangp/mnist/gradcam/run2/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run2/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run2/
+mv runs/* weights/xwgangp/mnist/gradcam/run2/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run3/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run3/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run3/
-mv runs/* weights/xwgangp/gradcam/mnist/run3/
+mkdir weights/xwgangp/mnist/gradcam/run3/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run3/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run3/
+mv runs/* weights/xwgangp/mnist/gradcam/run3/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run4/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run4/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run4/
-mv runs/* weights/xwgangp/gradcam/mnist/run4/
+mkdir weights/xwgangp/mnist/gradcam/run4/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run4/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run4/
+mv runs/* weights/xwgangp/mnist/gradcam/run4/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run5/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run5/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run5/
-mv runs/* weights/xwgangp/gradcam/mnist/run5/
+mkdir weights/xwgangp/mnist/gradcam/run5/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run5/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run5/
+mv runs/* weights/xwgangp/mnist/gradcam/run5/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run6/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run6/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run6/
-mv runs/* weights/xwgangp/gradcam/mnist/run6/
+mkdir weights/xwgangp/mnist/gradcam/run6/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run6/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run6/
+mv runs/* weights/xwgangp/mnist/gradcam/run6/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run7/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run7/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run7/
-mv runs/* weights/xwgangp/gradcam/mnist/run7/
+mkdir weights/xwgangp/mnist/gradcam/run7/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run7/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run7/
+mv runs/* weights/xwgangp/mnist/gradcam/run7/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run8/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run8/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run8/
-mv runs/* weights/xwgangp/gradcam/mnist/run8/
+mkdir weights/xwgangp/mnist/gradcam/run8/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run8/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run8/
+mv runs/* weights/xwgangp/mnist/gradcam/run8/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run9/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run9/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run9/
-mv runs/* weights/xwgangp/gradcam/mnist/run9/
+mkdir weights/xwgangp/mnist/gradcam/run9/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run9/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run9/
+mv runs/* weights/xwgangp/mnist/gradcam/run9/
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/gradcam/mnist/run10/
-mv weights/xwgangp/gradcam/mnist/disc_epoch_100.pth weights/xwgangp/gradcam/mnist/run10/
-mv weights/xwgangp/gradcam/mnist/gen_epoch_100.pth weights/xwgangp/gradcam/mnist/run10/
-mv runs/* weights/xwgangp/gradcam/mnist/run10/
+mkdir weights/xwgangp/mnist/gradcam/run10/
+mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run10/
+mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run10/
+mv runs/* weights/xwgangp/mnist/gradcam/run10/
