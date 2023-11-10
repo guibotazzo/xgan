2fb1d2495c362a0f01fe590a84441bd8f5d386d8
guilherme <guilherme.botazzo@unesp.br>
2023-11-10 20:19:44 +0100

    lil adjustments

--- script.sh
+++ script.sh
@@ -21,12 +21,12 @@
 # mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run2/
 # mv runs/* weights/xwgangp/mnist/gradcam/run2/
 
-python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/mnist/gradcam/run3/
-mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run3/
-mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run3/
-mv runs/* weights/xwgangp/mnist/gradcam/run3/
-rm weights/xwgangp/mnist/gradcam/*.pth
+# python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
+# mkdir weights/xwgangp/mnist/gradcam/run3/
+# mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run3/
+# mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run3/
+# mv runs/* weights/xwgangp/mnist/gradcam/run3/
+# rm weights/xwgangp/mnist/gradcam/*.pth
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
 mkdir weights/xwgangp/mnist/gradcam/run4/
