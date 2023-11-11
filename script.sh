bf4675c82db33b8c80313fc148a87815dd866d0d
guilherme <guilherme.botazzo@unesp.br>
2023-11-11 10:29:18 +0100

    lil adjustments

--- script.sh
+++ script.sh
@@ -28,26 +28,26 @@
 # mv runs/* weights/xwgangp/mnist/gradcam/run3/
 # rm weights/xwgangp/mnist/gradcam/*.pth
 
-python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/mnist/gradcam/run4/
-mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run4/
-mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run4/
-mv runs/* weights/xwgangp/mnist/gradcam/run4/
-rm weights/xwgangp/mnist/gradcam/*.pth
+# python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
+# mkdir weights/xwgangp/mnist/gradcam/run4/
+# mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run4/
+# mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run4/
+# mv runs/* weights/xwgangp/mnist/gradcam/run4/
+# rm weights/xwgangp/mnist/gradcam/*.pth
 
-python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/mnist/gradcam/run5/
-mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run5/
-mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run5/
-mv runs/* weights/xwgangp/mnist/gradcam/run5/
-rm weights/xwgangp/mnist/gradcam/*.pth
+# python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
+# mkdir weights/xwgangp/mnist/gradcam/run5/
+# mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run5/
+# mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run5/
+# mv runs/* weights/xwgangp/mnist/gradcam/run5/
+# rm weights/xwgangp/mnist/gradcam/*.pth
 
-python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
-mkdir weights/xwgangp/mnist/gradcam/run6/
-mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run6/
-mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run6/
-mv runs/* weights/xwgangp/mnist/gradcam/run6/
-rm weights/xwgangp/mnist/gradcam/*.pth
+# python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
+# mkdir weights/xwgangp/mnist/gradcam/run6/
+# mv weights/xwgangp/mnist/gradcam/disc_epoch_100.pth weights/xwgangp/mnist/gradcam/run6/
+# mv weights/xwgangp/mnist/gradcam/gen_epoch_100.pth weights/xwgangp/mnist/gradcam/run6/
+# mv runs/* weights/xwgangp/mnist/gradcam/run6/
+# rm weights/xwgangp/mnist/gradcam/*.pth
 
 python xwgangp.py -x gradcam -d mnist -s 28 -c 1 -e 100
 mkdir weights/xwgangp/mnist/gradcam/run7/
