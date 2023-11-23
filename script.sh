0e368393a6ff39bd11ae152183c048c9623b24ba
guilherme <guilherme.botazzo@unesp.br>
2023-11-23 08:59:19 +0100

    updated script

--- script.sh
+++ script.sh
@@ -1,48 +1,10 @@
 #!/bin/bash
 
-###############################
-# DCGAN (inputxgrad) on CIFAR10
-###############################
-
-GAN='DCGAN'
-XAI='inputxgrad'
-DATASET='cifar10'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
-
-for i in 1 2 3 4 5
-do
-python train.py --gan $GAN -d $DATASET --xai $XAI
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
-
-#################################
-# WGAN-GP (saliency) on CIFAR10
-#################################
-
-GAN='WGAN-GP'
-XAI='saliency'
-DATASET='cifar10'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
-
-for i in 1 2 3 4 5
-do
-python train.py --gan $GAN -d $DATASET --xai $XAI
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
-
-##################################
-## WGAN-GP (inputxgrad) on CIFAR10
-##################################
+################################
+## DCGAN (inputxgrad) on CIFAR10
+################################
 #
-#GAN='WGAN-GP'
+#GAN='DCGAN'
 #XAI='inputxgrad'
 #DATASET='cifar10'
 #FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
@@ -56,3 +18,41 @@ done
 #mv runs/* $FOLDER'run'$i'/'
 #rm $FOLDER'*.pth'
 #done
+#
+##################################
+## WGAN-GP (saliency) on CIFAR10
+##################################
+#
+#GAN='WGAN-GP'
+#XAI='saliency'
+#DATASET='cifar10'
+#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+#
+#for i in 1 2 3 4 5
+#do
+#python train.py --gan $GAN -d $DATASET --xai $XAI
+#mkdir $FOLDER'run'$i'/'
+#mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+#mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+#mv runs/* $FOLDER'run'$i'/'
+#rm $FOLDER'*.pth'
+#done
+
+#################################
+# WGAN-GP (inputxgrad) on CIFAR10
+#################################
+
+GAN='WGAN-GP'
+XAI='inputxgrad'
+DATASET='cifar10'
+FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+
+for i in 1 2 3 4 5
+do
+python train.py --gan $GAN -d $DATASET --xai $XAI
+mkdir $FOLDER'run'$i'/'
+mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+mv runs/* $FOLDER'run'$i'/'
+rm $FOLDER'*.pth'
+done
