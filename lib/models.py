38bb7fc1369ef31847ac9d76753a1dfd14c4dee2
guilherme <guilherme.botazzo@unesp.br>
2023-11-24 12:18:55 +0100

    teste

--- lib/models.py
+++ lib/models.py
@@ -250,6 +250,63 @@ class DiscriminatorMNIST(nn.Module):
         return output.view(-1, 1).squeeze(1)
 
 
+class GeneratorCIFAR(nn.Module):
+    def __init__(self, noise_dim, channels, feature_maps):
+        super(GeneratorCIFAR, self).__init__()
+        self.nz = noise_dim  # Size of z latent vector
+        self.ngf = feature_maps  # Size of feature maps in generator
+        self.nc = channels  # Number of channels in the training images
+        self.network = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=self.nz, out_channels=self.ngf*8, kernel_size=3, stride=1, padding=0,
+                               bias=False),
+            nn.BatchNorm2d(self.ngf*8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(in_channels=self.ngf*8, out_channels=self.ngf*4, kernel_size=3, stride=2, padding=1,
+                               bias=False),
+            nn.BatchNorm2d(self.ngf*4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(in_channels=self.ngf*4, out_channels=self.ngf*2, kernel_size=3, stride=2, padding=1,
+                               bias=False),
+            nn.BatchNorm2d(self.ngf*2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(in_channels=self.ngf*2, out_channels=self.ngf, kernel_size=3, stride=2, padding=1,
+                               bias=False),
+            nn.BatchNorm2d(self.ngf),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(in_channels=self.ngf, out_channels=self.nc, kernel_size=2, stride=2, padding=1,
+                               bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, noise):
+        return self.network(noise)
+
+
+class DiscriminatorCIFAR(nn.Module):
+    def __init__(self, channels, feature_maps):
+        super(DiscriminatorCIFAR, self).__init__()
+        self.ndf = feature_maps  # Size of feature maps in discriminator
+        self.nc = channels  # Number of channels of the training images
+        self.network = nn.Sequential(
+            nn.Conv2d(in_channels=self.nc, out_channels=self.ndf, kernel_size=2, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(in_channels=self.ndf, out_channels=self.ndf*2, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(self.ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(in_channels=self.ndf*2, out_channels=self.ndf*4, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(self.ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(in_channels=self.ndf*4, out_channels=self.ndf*8, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(self.ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(in_channels=self.ndf*8, out_channels=1, kernel_size=3, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img):
+        return self.network(img)
+
+
 ###########################
 # Models for classification
 ###########################
