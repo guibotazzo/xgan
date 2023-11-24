4bac0aab3ac2995397d154d70b5ce6d9290f3de9
guilherme <guilherme.botazzo@unesp.br>
2023-11-24 01:12:49 +0100

    script

--- script.sh
+++ script.sh
@@ -19,35 +19,16 @@
 #rm $FOLDER'*.pth'
 #done
 #
-##################################
-## WGAN-GP (saliency) on CIFAR10
-##################################
-#
-#GAN='WGAN-GP'
-#XAI='saliency'
-#DATASET='cifar10'
-#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
-#
-#for i in 1 2 3 4 5
-#do
-#python train.py --gan $GAN -d $DATASET --xai $XAI
-#mkdir $FOLDER'run'$i'/'
-#mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-#mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-#mv runs/* $FOLDER'run'$i'/'
-#rm $FOLDER'*.pth'
-#done
-
 #################################
-# WGAN-GP (inputxgrad) on CIFAR10
+# WGAN-GP (saliency) on CIFAR10
 #################################
 
 GAN='WGAN-GP'
-XAI='inputxgrad'
+XAI='saliency'
 DATASET='cifar10'
 FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
 
-for i in 4 5
+for i in 2 3 4 5
 do
 python train.py --gan $GAN -d $DATASET --xai $XAI
 mkdir $FOLDER'run'$i'/'
@@ -56,3 +37,22 @@ mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
 mv runs/* $FOLDER'run'$i'/'
 rm $FOLDER'*.pth'
 done
+
+#################################
+# WGAN-GP (inputxgrad) on CIFAR10
+#################################
+
+#GAN='WGAN-GP'
+#XAI='inputxgrad'
+#DATASET='cifar10'
+#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+#
+#for i in 4 5
+#do
+#python train.py --gan $GAN -d $DATASET --xai $XAI
+#mkdir $FOLDER'run'$i'/'
+#mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+#mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+#mv runs/* $FOLDER'run'$i'/'
+#rm $FOLDER'*.pth'
+#done
