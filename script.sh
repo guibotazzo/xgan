33bf68cf01118300d1dcfd2ff664d03e58669342
guilherme <guilherme.botazzo@unesp.br>
2023-11-25 21:31:54 +0000

    teste

--- script.sh
+++ script.sh
@@ -26,11 +26,11 @@ done
 GAN='DCGAN'
 XAI='saliency'
 DATASET='cifar10'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+FOLDER='weights/'$GAN'/'$DATASET'/'
 
 for i in 1 2 3 4 5
 do
-python train.py --gan $GAN -d $DATASET --xai $XAI
+python train.py --gan $GAN -d $DATASET
 mkdir $FOLDER'run'$i'/'
 mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
 mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
