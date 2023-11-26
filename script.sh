9f63be9e0d2d08cbb52e9c5513615d3d7d1381e4
guilherme <guilherme.botazzo@unesp.br>
2023-11-26 18:14:25 +0000

    teste

--- script.sh
+++ script.sh
@@ -5,26 +5,6 @@
 ###############################
 
 GAN='DCGAN'
-XAI='none'
-DATASET='cifar10'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
-
-for i in 1 2 3 4 5
-do
-python train.py --gan $GAN -d $DATASET --xai $XAI
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
-
-###############################
-# DCGAN (inputxgrad) on CIFAR10
-###############################
-
-GAN='DCGAN'
-XAI='saliency'
 DATASET='cifar10'
 FOLDER='weights/'$GAN'/'$DATASET'/'
 
@@ -42,20 +22,39 @@ done
 # DCGAN (inputxgrad) on CIFAR10
 ###############################
 
-GAN='DCGAN'
-XAI='deeplift'
-DATASET='cifar10'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+#GAN='DCGAN'
+#XAI='saliency'
+#DATASET='cifar10'
+#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+#
+#for i in 1 2 3 4 5
+#do
+#python train.py --gan $GAN -d $DATASET
+#mkdir $FOLDER'run'$i'/'
+#mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+#mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+#mv runs/* $FOLDER'run'$i'/'
+#rm $FOLDER'*.pth'
+#done
 
-for i in 1 2 3 4 5
-do
-python train.py --gan $GAN -d $DATASET --xai $XAI
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
+###############################
+# DCGAN (inputxgrad) on CIFAR10
+###############################
+
+#GAN='DCGAN'
+#XAI='deeplift'
+#DATASET='cifar10'
+#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+#
+#for i in 1 2 3 4 5
+#do
+#python train.py --gan $GAN -d $DATASET --xai $XAI
+#mkdir $FOLDER'run'$i'/'
+#mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+#mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+#mv runs/* $FOLDER'run'$i'/'
+#rm $FOLDER'*.pth'
+#done
 
 #################################
 # WGAN-GP (saliency) on CIFAR10
