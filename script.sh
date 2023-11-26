359a39e1390574f5b370c811b553ca08f890abf9
guilherme <guilherme.botazzo@unesp.br>
2023-11-26 23:43:25 +0000

    teste

--- script.sh
+++ script.sh
@@ -4,28 +4,9 @@
 # DCGAN (inputxgrad) on CIFAR10
 ###############################
 
-GAN='DCGAN'
-DATASET='cifar10'
-FOLDER='weights/'$GAN'/'$DATASET'/'
-
-for i in 1 2 3 4 5
-do
-python train.py --gan $GAN -d $DATASET
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
-
-###############################
-# DCGAN (inputxgrad) on CIFAR10
-###############################
-
 #GAN='DCGAN'
-#XAI='saliency'
 #DATASET='cifar10'
-#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+#FOLDER='weights/'$GAN'/'$DATASET'/'
 #
 #for i in 1 2 3 4 5
 #do
@@ -41,20 +22,39 @@ done
 # DCGAN (inputxgrad) on CIFAR10
 ###############################
 
-#GAN='DCGAN'
-#XAI='deeplift'
-#DATASET='cifar10'
-#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
-#
-#for i in 1 2 3 4 5
-#do
-#python train.py --gan $GAN -d $DATASET --xai $XAI
-#mkdir $FOLDER'run'$i'/'
-#mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-#mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-#mv runs/* $FOLDER'run'$i'/'
-#rm $FOLDER'*.pth'
-#done
+GAN='DCGAN'
+XAI='saliency'
+DATASET='cifar10'
+FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+
+for i in 1 2 3 4 5
+do
+python train.py --gan $GAN -d $DATASET
+mkdir $FOLDER'run'$i'/'
+mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+mv runs/* $FOLDER'run'$i'/'
+rm $FOLDER'*.pth'
+done
+
+###############################
+# DCGAN (inputxgrad) on CIFAR10
+###############################
+
+GAN='DCGAN'
+XAI='deeplift'
+DATASET='cifar10'
+FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+
+for i in 1 2 3 4 5
+do
+python train.py --gan $GAN -d $DATASET --xai $XAI
+mkdir $FOLDER'run'$i'/'
+mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+mv runs/* $FOLDER'run'$i'/'
+rm $FOLDER'*.pth'
+done
 
 #################################
 # WGAN-GP (saliency) on CIFAR10
