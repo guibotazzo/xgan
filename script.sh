13a6c122014bdc3bcf1d7baba0b9b5f442ecd939
guilherme <guilherme.botazzo@unesp.br>
2023-11-27 18:42:05 +0000

    teste

--- script.sh
+++ script.sh
@@ -22,27 +22,27 @@
 # DCGAN (inputxgrad) on CIFAR10
 ###############################
 
-GAN='DCGAN'
-XAI='saliency'
-DATASET='cifar10'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
-
-for i in 1 2 3 4 5
-do
-python train.py --gan $GAN -d $DATASET
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
+#GAN='DCGAN'
+#XAI='saliency'
+#DATASET='cifar10'
+#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+#
+#for i in 1 2 3 4 5
+#do
+#python train.py --gan $GAN -d $DATASET
+#mkdir $FOLDER'run'$i'/'
+#mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+#mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+#mv runs/* $FOLDER'run'$i'/'
+#rm $FOLDER'*.pth'
+#done
 
 ###############################
 # DCGAN (inputxgrad) on CIFAR10
 ###############################
 
 GAN='DCGAN'
-XAI='deeplift'
+XAI='saliency'
 DATASET='cifar10'
 FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
 
