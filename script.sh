ec67ea000a167a39ea74f82171eeca23ac7c8cb6
guilherme <guilherme.botazzo@unesp.br>
2023-12-12 09:21:13 +0100

    script

--- script.sh
+++ script.sh
@@ -18,20 +18,20 @@
 #rm $FOLDER'*.pth'
 #done
 
-GAN='RaSGAN'
-DATASET='mnist'
-XAI='saliency'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
-
-for i in 3 4 5
-do
-python train.py --gan $GAN -d $DATASET -s 32 -c 1 --xai $XAI
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
+#GAN='RaSGAN'
+#DATASET='mnist'
+#XAI='saliency'
+#FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+#
+#for i in 3 4 5
+#do
+#python train.py --gan $GAN -d $DATASET -s 32 -c 1 --xai $XAI
+#mkdir $FOLDER'run'$i'/'
+#mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
+#mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
+#mv runs/* $FOLDER'run'$i'/'
+#rm $FOLDER'*.pth'
+#done
 
 GAN='RaSGAN'
 DATASET='mnist'
