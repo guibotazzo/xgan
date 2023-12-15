1867e6a649df2c294a785f7e71a568671cf72283
guilherme <guilherme.botazzo@unesp.br>
2023-12-15 11:39:42 +0100

    add create dataset

--- lib/create_dataset.py
+++ lib/create_dataset.py
@@ -4,58 +4,28 @@ import argparse
 import pathlib
 import models
 from torchvision.utils import save_image
-import numpy as np
-from torch.autograd import Variable
 
 
-# def _create_mnist(args, device):
-#     path = '/Users/guilherme/datasets/artificial/' + args.model + '/' + args.dataset + '/'
-#
-#     generator = models.GeneratorACGAN(n_classes=10, z_dim=100, img_size=32, channels=1).to(device)
-#     # generator = models.Generator28(noise_dim=100, channels=1, feature_maps=64).to(device)
-#     generator.load_state_dict(
-#         torch.load('/Users/guilherme/Documents/Doutorado/Sources/xgan/weights/xacgan/mnist/saliency/gen_epoch_10.pth',
-#                    map_location=torch.device('cpu')))
-#
-#     float_tensor = torch.cuda.FloatTensor if device == torch.device('cuda') else torch.FloatTensor
-#     long_tensor = torch.cuda.LongTensor if device == torch.device('cuda') else torch.LongTensor
-#
-#     labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-#
-#     for label in labels:
-#         if not os.path.exists(path + label):
-#             folder = path + label
-#             folder = pathlib.Path(folder)
-#             folder.mkdir(parents=True)
-#
-#         output_folder = path + label + '/'
-#
-#         for i in range(1, 6000):
-#             # noise = torch.randn(1, 100, 1, 1, device=device)
-#             # fake = generator(noise)
-#             # save_image(fake, output_folder + f'{i:04d}' + '.png', normalize=True)
-#
-#             z = Variable(float_tensor(np.random.normal(0, 1, (64, args.noise_dim)))).to(device)
-#             gen_labels = Variable(long_tensor(np.random.randint(0, args.n_classes, 64))).to(device)
-#
-#             # Generate a batch of images
-#             gen_imgs = generator(z, gen_labels)
-#
-#             save_image(fake, output_folder + f'{i:04d}' + '.png', normalize=True)
+def _create_dataset(args):
+    device = torch.device('cpu')
 
+    if args.xai == 'none':
+        pathh = 'weights/' + args.gan + '/' + args.dataset + '/'
+    else:
+        pathh = 'weights/' + args.gan + '/' + args.dataset + '/' + args.xai + '/'
 
-def _create_mnist(args, device):
-    num_imgs = 6000
-    batch = 64
-    labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    path = '/Users/guilherme/datasets/artificial/' + args.model + '/' + args.dataset + '/'
+    generator = models.Generator(args).apply(models.weights_init).to(device)
 
-    generator = models.GeneratorACGAN(n_classes=10, z_dim=100, img_size=32, channels=1).to(device)
-    weights_path = '/Users/guilherme/Documents/Doutorado/Sources/xgan/weights/xacgan/mnist/deeplift/gen_epoch_10.pth'
-    generator.load_state_dict(torch.load(weights_path, map_location=device))
+    if args.dataset == 'ucsb' or args.dataset == 'cr':
+        labels = ['Benign', 'Malignant']
+    elif args.dataset == 'la':
+        labels = ['1', '2', '3', '4']
+    elif args.dataset == 'lg':
+        labels = ['Class 1', 'Class 2']
+    else:
+        labels = ['cll', 'fl', 'mcl']
 
-    float_tensor = torch.cuda.FloatTensor if device == torch.device('cuda') else torch.FloatTensor
-    long_tensor = torch.cuda.LongTensor if device == torch.device('cuda') else torch.LongTensor
+    path = '../datasets/artificial/' + args.dataset.upper() + str(args.image_size) + '/'
 
     for label in labels:
         if not os.path.exists(path + label):
@@ -63,65 +33,52 @@ def _create_mnist(args, device):
             folder = pathlib.Path(folder)
             folder.mkdir(parents=True)
 
-        output_folder = path + label + '/'
+        weights_path = pathh + label + f'/gen_epoch_{args.epoch:d}.pth'
 
-        i = 1
-        for _ in range(int(num_imgs/batch)):
-            # noise = torch.randn(batch, 100, 1, 1, device=device)
-            # fake = generator(noise)
-
-            z = Variable(float_tensor(np.random.normal(0, 1, (batch, 100)))).to(device)
-            label_array = np.ones(batch, dtype=int) * int(label)
-            gen_labels = Variable(long_tensor(label_array)).to(device)
-
-            fake = generator(z, gen_labels)
-
-            for j in range(batch):
-                save_image(fake[j], output_folder + f'{i:04d}' + '.png', normalize=True)
-                i += 1
-
-
-def _create_nhl256(args, device):
-    num_imgs = 1024
-    batch = 32
-    labels = ['cll', 'fl', 'mcl']
-    path = '/Users/guilherme/datasets/artificial' + args.model + '/' + args.dataset + '/'
-
-    generator = models.Generator256(noise_dim=100, channels=3, feature_maps=16)
-
-    for label in labels:
-        if not os.path.exists(path + label):
-            folder = path + label
-            folder = pathlib.Path(folder)
-            folder.mkdir(parents=True)
-
-        generator.load_state_dict(torch.load(
-            '/Users/guilherme/Documents/Doutorado/Sources/xdcgan/weights/xwgan-gp/nhl256/' + label + '_gen',
-            map_location=torch.device('cpu')
-        ))
+        generator.load_state_dict(torch.load(weights_path, map_location=device))
 
         output_folder = path + label + '/'
 
         i = 1
-        for _ in range(int(num_imgs/batch)):
-            noise = torch.randn(batch, 100, 1, 1, device=device)
+        for _ in range(int(args.num_imgs/args.batch_size)):
+            noise = torch.randn(args.batch_size, args.z_size, 1, 1, device=device)
             fake = generator(noise)
 
-            for j in range(batch):
+            for j in range(args.batch_size):
                 save_image(fake[j], output_folder + f'{i:04d}' + '.png', normalize=True)
                 i += 1
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a artificial dataset')
-    parser.add_argument('--dataset', '-d', type=str, choices=['mnist', 'nhl256'], default='mnist')
-    parser.add_argument('--model', '-m', type=str, choices=['dcgan', 'xdcgan', 'xwgan-gp', 'xacgan'], default='dcgan')
+    parser.add_argument('--dataset', '-d', type=str, choices=['cr', 'ucsb', 'la', 'lg', 'nhl'])
+    parser.add_argument('--image_size', '-s', type=int, default=64)
+    parser.add_argument('--num_imgs', '-n', type=int, default=1000)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--epoch', '-e', type=int, default=100)
+    parser.add_argument('--gan', type=str,
+                        choices=['DCGAN', 'LSGAN', 'WGAN-GP', 'HingeGAN', 'RSGAN', 'RaSGAN', 'RaLSGAN', 'RaHingeGAN'],
+                        default='DCGAN')
+    parser.add_argument('--xai', '-x', type=str, choices=['none', 'saliency', 'deeplift', 'inputxgrad'], default='none')
+
+    ######################
+    # Generator parameters
+    ######################
+    parser.add_argument('--z_size', type=int, default=128)
+    parser.add_argument('--G_h_size', type=int, default=128, help='Number of feature maps')
+    parser.add_argument('--lr_G', type=float, default=.0001, help='Generator learning rate')
+    parser.add_argument('--Giters', type=int, default=1, help='Number of iterations of G.')
+    parser.add_argument('--spectral_G', type=bool, default=False,
+                        help='Use spectral norm. to make the generator Lipschitz (Generally only D is spectral).')
+    parser.add_argument('--no_batch_norm_G', type=bool, default=False, help='If True, no batch norm in G.')
+    parser.add_argument('--Tanh_GD', type=bool, default=False, help='If True, tanh everywhere.')
+    parser.add_argument('--SELU', type=bool, default=False,
+                        help='Use SELU instead of ReLU with BatchNorm. This improves stability.')
+    parser.add_argument("--NN_conv", type=bool, default=False,
+                        help="Uses nearest-neighbor resized convolutions instead of strided convolutions")
+    parser.add_argument('--penalty', type=float, default=10, help='Gradient penalty parameter for WGAN-GP')
+    parser.add_argument('--grad_penalty', type=bool, default=False,
+                        help='If True, use gradient penalty of WGAN-GP but with whichever gan chosen.')
     arguments = parser.parse_args()
 
-    dev = torch.device('cpu')
-
-    if arguments.dataset == 'mnist':
-        _create_mnist(arguments, dev)
-
-    if arguments.dataset == 'nhl256':
-        _create_nhl256(arguments, dev)
+    _create_dataset(arguments)
