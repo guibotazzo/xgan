0ea03eb16e6f4d6f7d8ca2932a7f1c8d7e89ce16
guilherme <guilherme.botazzo@unesp.br>
2024-01-11 14:59:15 -0300

    oye

--- script.sh
+++ script.sh
@@ -33,32 +33,14 @@
 #rm $FOLDER'*.pth'
 #done
 
-GAN='RaSGAN'
-DATASET='mnist'
-XAI='deeplift'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
+# CR
+python train.py --gan WGAN-GP --xai saliency -d cr -s 64 -c 3
+python train.py --gan WGAN-GP --xai deeplift -d cr -s 64 -c 3
+python train.py --gan WGAN-GP --xai inputxgrad -d cr -s 64 -c 3
+python train.py --gan WGAN-GP -d cr -s 64 -c 3
 
-for i in 3
-do
-python train.py --gan $GAN -d $DATASET -s 32 -c 1 --xai $XAI
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
-
-GAN='RaSGAN'
-DATASET='mnist'
-XAI='inputxgrad'
-FOLDER='weights/'$GAN'/'$DATASET'/'$XAI'/'
-
-for i in 1 2 3
-do
-python train.py --gan $GAN -d $DATASET -s 32 -c 1 --xai $XAI
-mkdir $FOLDER'run'$i'/'
-mv $FOLDER'disc_epoch_100.pth' $FOLDER'run'$i'/'
-mv $FOLDER'gen_epoch_100.pth' $FOLDER'run'$i'/'
-mv runs/* $FOLDER'run'$i'/'
-rm $FOLDER'*.pth'
-done
+# UCSB
+python train.py --gan WGAN-GP --xai saliency -d ucsb -s 64 -c 3
+python train.py --gan WGAN-GP --xai deeplift -d ucsb -s 64 -c 3
+python train.py --gan WGAN-GP --xai inputxgrad -d ucsb -s 64 -c 3
+python train.py --gan WGAN-GP -d ucsb -s 64 -c 3
