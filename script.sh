95cbbd2014247aa62c3fbfbfefb87006e8b2e946
guilherme <guilherme.botazzo@unesp.br>
2024-01-26 11:49:42 -0300

    oye

--- script.sh
+++ script.sh
@@ -14,188 +14,188 @@
 #rm $FOLDER'*.pth'
 #done
 
-# -- CR - SALIENCY --------------
-# -------- 1 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/2
-rm -r datasets/LA64/3
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai saliency -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/saliency/1
-mv runs/* weights/WGAN-GP/la/saliency/1
-mv weights/WGAN-GP/la/saliency/gen_epoch_100.pth weights/WGAN-GP/la/saliency/1
-mv weights/WGAN-GP/la/saliency/disc_epoch_100.pth weights/WGAN-GP/la/saliency/1
-
-# -------- 2 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/3
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai saliency -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/saliency/2
-mv runs/* weights/WGAN-GP/la/saliency/2
-mv weights/WGAN-GP/la/saliency/gen_epoch_100.pth weights/WGAN-GP/la/saliency/2
-mv weights/WGAN-GP/la/saliency/disc_epoch_100.pth weights/WGAN-GP/la/saliency/2
-
-# -------- 3 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/2
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai saliency -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/saliency/3
-mv runs/* weights/WGAN-GP/la/saliency/3
-mv weights/WGAN-GP/la/saliency/gen_epoch_100.pth weights/WGAN-GP/la/saliency/3
-mv weights/WGAN-GP/la/saliency/disc_epoch_100.pth weights/WGAN-GP/la/saliency/3
-
-# -------- 4 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/2
-rm -r datasets/LA64/3
-
-python train.py --gan WGAN-GP --xai saliency -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/saliency/4
-mv runs/* weights/WGAN-GP/la/saliency/4
-mv weights/WGAN-GP/la/saliency/gen_epoch_100.pth weights/WGAN-GP/la/saliency/4
-mv weights/WGAN-GP/la/saliency/disc_epoch_100.pth weights/WGAN-GP/la/saliency/4
-
-# -- CR - DEEPLIFT --------------
-# -------- 1 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/2
-rm -r datasets/LA64/3
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai deeplift -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/deeplift/1
-mv runs/* weights/WGAN-GP/la/deeplift/1
-mv weights/WGAN-GP/la/deeplift/gen_epoch_100.pth weights/WGAN-GP/la/deeplift/1
-mv weights/WGAN-GP/la/deeplift/disc_epoch_100.pth weights/WGAN-GP/la/deeplift/1
-
-# -------- 2 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/3
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai deeplift -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/deeplift/2
-mv runs/* weights/WGAN-GP/la/deeplift/2
-mv weights/WGAN-GP/la/deeplift/gen_epoch_100.pth weights/WGAN-GP/la/deeplift/2
-mv weights/WGAN-GP/la/deeplift/disc_epoch_100.pth weights/WGAN-GP/la/deeplift/2
-
-# -------- 3 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/2
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai deeplift -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/deeplift/3
-mv runs/* weights/WGAN-GP/la/deeplift/3
-mv weights/WGAN-GP/la/deeplift/gen_epoch_100.pth weights/WGAN-GP/la/deeplift/3
-mv weights/WGAN-GP/la/deeplift/disc_epoch_100.pth weights/WGAN-GP/la/deeplift/3
-
-# -------- 4 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/2
-rm -r datasets/LA64/3
-
-python train.py --gan WGAN-GP --xai deeplift -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/deeplift/4
-mv runs/* weights/WGAN-GP/la/deeplift/4
-mv weights/WGAN-GP/la/deeplift/gen_epoch_100.pth weights/WGAN-GP/la/deeplift/4
-mv weights/WGAN-GP/la/deeplift/disc_epoch_100.pth weights/WGAN-GP/la/deeplift/4
-
-# -- CR - INPUTXGRAD --------------
-# -------- 1 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/2
-rm -r datasets/LA64/3
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai inputxgrad -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/inputxgrad/1
-mv runs/* weights/WGAN-GP/la/inputxgrad/1
-mv weights/WGAN-GP/la/inputxgrad/gen_epoch_100.pth weights/WGAN-GP/la/inputxgrad/1
-mv weights/WGAN-GP/la/inputxgrad/disc_epoch_100.pth weights/WGAN-GP/la/inputxgrad/1
-
-# -------- 2 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/3
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai inputxgrad -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/inputxgrad/2
-mv runs/* weights/WGAN-GP/la/inputxgrad/2
-mv weights/WGAN-GP/la/inputxgrad/gen_epoch_100.pth weights/WGAN-GP/la/inputxgrad/2
-mv weights/WGAN-GP/la/inputxgrad/disc_epoch_100.pth weights/WGAN-GP/la/inputxgrad/2
-
-# -------- 3 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/2
-rm -r datasets/LA64/4
-
-python train.py --gan WGAN-GP --xai inputxgrad -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/inputxgrad/3
-mv runs/* weights/WGAN-GP/la/inputxgrad/3
-mv weights/WGAN-GP/la/inputxgrad/gen_epoch_100.pth weights/WGAN-GP/la/inputxgrad/3
-mv weights/WGAN-GP/la/inputxgrad/disc_epoch_100.pth weights/WGAN-GP/la/inputxgrad/3
-
-# -------- 4 --------
-rm -r datasets/LA64
-unzip -q datasets/LA64_original.zip
-mv LA64 datasets
-rm -r datasets/LA64/1
-rm -r datasets/LA64/2
-rm -r datasets/LA64/3
-
-python train.py --gan WGAN-GP --xai inputxgrad -d la -s 64 -c 3
-
-mkdir weights/WGAN-GP/la/inputxgrad/4
-mv runs/* weights/WGAN-GP/la/inputxgrad/4
-mv weights/WGAN-GP/la/inputxgrad/gen_epoch_100.pth weights/WGAN-GP/la/inputxgrad/4
-mv weights/WGAN-GP/la/inputxgrad/disc_epoch_100.pth weights/WGAN-GP/la/inputxgrad/4
+## -- CR - SALIENCY --------------
+## -------- 1 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/3
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai saliency -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/saliency/1
+#mv runs/* weights/WGAN-GP/la/saliency/1
+#mv weights/WGAN-GP/la/saliency/gen_epoch_100.pth weights/WGAN-GP/la/saliency/1
+#mv weights/WGAN-GP/la/saliency/disc_epoch_100.pth weights/WGAN-GP/la/saliency/1
+#
+## -------- 2 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/3
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai saliency -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/saliency/2
+#mv runs/* weights/WGAN-GP/la/saliency/2
+#mv weights/WGAN-GP/la/saliency/gen_epoch_100.pth weights/WGAN-GP/la/saliency/2
+#mv weights/WGAN-GP/la/saliency/disc_epoch_100.pth weights/WGAN-GP/la/saliency/2
+#
+## -------- 3 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai saliency -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/saliency/3
+#mv runs/* weights/WGAN-GP/la/saliency/3
+#mv weights/WGAN-GP/la/saliency/gen_epoch_100.pth weights/WGAN-GP/la/saliency/3
+#mv weights/WGAN-GP/la/saliency/disc_epoch_100.pth weights/WGAN-GP/la/saliency/3
+#
+## -------- 4 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/3
+#
+#python train.py --gan WGAN-GP --xai saliency -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/saliency/4
+#mv runs/* weights/WGAN-GP/la/saliency/4
+#mv weights/WGAN-GP/la/saliency/gen_epoch_100.pth weights/WGAN-GP/la/saliency/4
+#mv weights/WGAN-GP/la/saliency/disc_epoch_100.pth weights/WGAN-GP/la/saliency/4
+#
+## -- CR - DEEPLIFT --------------
+## -------- 1 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/3
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai deeplift -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/deeplift/1
+#mv runs/* weights/WGAN-GP/la/deeplift/1
+#mv weights/WGAN-GP/la/deeplift/gen_epoch_100.pth weights/WGAN-GP/la/deeplift/1
+#mv weights/WGAN-GP/la/deeplift/disc_epoch_100.pth weights/WGAN-GP/la/deeplift/1
+#
+## -------- 2 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/3
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai deeplift -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/deeplift/2
+#mv runs/* weights/WGAN-GP/la/deeplift/2
+#mv weights/WGAN-GP/la/deeplift/gen_epoch_100.pth weights/WGAN-GP/la/deeplift/2
+#mv weights/WGAN-GP/la/deeplift/disc_epoch_100.pth weights/WGAN-GP/la/deeplift/2
+#
+## -------- 3 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai deeplift -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/deeplift/3
+#mv runs/* weights/WGAN-GP/la/deeplift/3
+#mv weights/WGAN-GP/la/deeplift/gen_epoch_100.pth weights/WGAN-GP/la/deeplift/3
+#mv weights/WGAN-GP/la/deeplift/disc_epoch_100.pth weights/WGAN-GP/la/deeplift/3
+#
+## -------- 4 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/3
+#
+#python train.py --gan WGAN-GP --xai deeplift -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/deeplift/4
+#mv runs/* weights/WGAN-GP/la/deeplift/4
+#mv weights/WGAN-GP/la/deeplift/gen_epoch_100.pth weights/WGAN-GP/la/deeplift/4
+#mv weights/WGAN-GP/la/deeplift/disc_epoch_100.pth weights/WGAN-GP/la/deeplift/4
+#
+## -- CR - INPUTXGRAD --------------
+## -------- 1 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/3
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai inputxgrad -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/inputxgrad/1
+#mv runs/* weights/WGAN-GP/la/inputxgrad/1
+#mv weights/WGAN-GP/la/inputxgrad/gen_epoch_100.pth weights/WGAN-GP/la/inputxgrad/1
+#mv weights/WGAN-GP/la/inputxgrad/disc_epoch_100.pth weights/WGAN-GP/la/inputxgrad/1
+#
+## -------- 2 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/3
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai inputxgrad -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/inputxgrad/2
+#mv runs/* weights/WGAN-GP/la/inputxgrad/2
+#mv weights/WGAN-GP/la/inputxgrad/gen_epoch_100.pth weights/WGAN-GP/la/inputxgrad/2
+#mv weights/WGAN-GP/la/inputxgrad/disc_epoch_100.pth weights/WGAN-GP/la/inputxgrad/2
+#
+## -------- 3 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/4
+#
+#python train.py --gan WGAN-GP --xai inputxgrad -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/inputxgrad/3
+#mv runs/* weights/WGAN-GP/la/inputxgrad/3
+#mv weights/WGAN-GP/la/inputxgrad/gen_epoch_100.pth weights/WGAN-GP/la/inputxgrad/3
+#mv weights/WGAN-GP/la/inputxgrad/disc_epoch_100.pth weights/WGAN-GP/la/inputxgrad/3
+#
+## -------- 4 --------
+#rm -r datasets/LA64
+#unzip -q datasets/LA64_original.zip
+#mv LA64 datasets
+#rm -r datasets/LA64/1
+#rm -r datasets/LA64/2
+#rm -r datasets/LA64/3
+#
+#python train.py --gan WGAN-GP --xai inputxgrad -d la -s 64 -c 3
+#
+#mkdir weights/WGAN-GP/la/inputxgrad/4
+#mv runs/* weights/WGAN-GP/la/inputxgrad/4
+#mv weights/WGAN-GP/la/inputxgrad/gen_epoch_100.pth weights/WGAN-GP/la/inputxgrad/4
+#mv weights/WGAN-GP/la/inputxgrad/disc_epoch_100.pth weights/WGAN-GP/la/inputxgrad/4
 
 
 # ------------------------
