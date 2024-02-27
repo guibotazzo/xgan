dec8ab0c56abc5dfd9e74327dd90b3dc429d9dd1
Guilherme Botazzo Rozendo <GuiRozendo@users.noreply.github.com>
2024-02-27 11:39:39 -0300

    Delete script.bat

--- script.bat
+++
@@ -1,9 +0,0 @@
-rmdir /s /q weights
-rmdir /s /q runs
-
-python train.py --gan WGAN-GP --xai deeplift
-mkdir weights\WGAN-GP\cifar10\deeplift\run1
-move weights\WGAN-GP\cifar10\deeplift\disc_epoch_100.pth weights\WGAN-GP\cifar10\deeplift\run1\
-move weights\WGAN-GP\cifar10\deeplift\gen_epoch_100.pth weights\WGAN-GP\cifar10\deeplift\run1\
-move runs\* weights\WGAN-GP\cifar10\deeplift\run1\
-del weights\WGAN-GP\cifar10\deeplift\*.pth
