74c7c013c2b4212e0d318745cdb91157408b372d
guilherme <guilherme.botazzo@unesp.br>
2024-04-25 16:28:18 -0300

    add kid metric

--- quality_eval.py
+++ quality_eval.py
@@ -4,12 +4,49 @@ from tqdm import tqdm
 from torchmetrics.image.fid import FrechetInceptionDistance
 from torchmetrics.image.inception import InceptionScore
 from torchmetrics.image.mifid import MemorizationInformedFrechetInceptionDistance
+from torchmetrics.image.kid import KernelInceptionDistance
 from lib import models, datasets, utils
 
 
 def _minmax_scaler(arr, *, vmin=0, vmax=255):
     arr_min, arr_max = arr.min(), arr.max()
     return ((arr - arr_min) / (arr_max - arr_min)) * (vmax - vmin) + vmin
+
+
+def _compute_kid(args, generator, dataset, device):
+    kid = KernelInceptionDistance(feature=2048).to(device)
+
+    end = int(50000/32)
+    i = 1
+    with tqdm(total=end, desc='Computing MIFID') as pbar:
+        for reals, _ in dataset:
+            reals = reals.to(device)
+            reals = _minmax_scaler(reals)
+
+            if args.channels == 1:
+                reals = reals.repeat(1, 3, 1, 1)
+
+            kid.update(reals.to(torch.uint8), real=True)
+
+            noise = torch.randn(args.batch_size, args.z_size, 1, 1, device=device)
+            fakes = generator(noise)
+            fakes = _minmax_scaler(fakes)
+
+            if args.channels == 1:
+                fakes = fakes.repeat(1, 3, 1, 1)
+
+            kid.update(fakes.to(torch.uint8), real=False)
+
+            pbar.update(1)
+            i = i + 1
+
+            if i == end:
+                break
+
+    print(f'KID ({args.gan}/{args.xai}/{args.label}): {kid.compute().detach().cpu().numpy()}')
+
+    with open(f'kid_{args.dataset}.csv', 'a') as file:
+        file.write(f'{args.gan},{args.xai},{args.label},{args.epoch},{kid.compute().detach().cpu().numpy()}\n')
 
 
 def _compute_mifid(args, generator, dataset, device):
@@ -131,11 +168,13 @@ def main(args):
         _compute_is(args, generator, dataset, device)
     elif args.metric == 'mifid':
         _compute_mifid(args, generator, dataset, device)
+    elif args.metric == 'kid':
+        _compute_kid(args, generator, dataset, device)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Evaluation metrics')
-    parser.add_argument('--metric', '-m', type=str, choices=['fid', 'is', 'mifid'], default='fid')
+    parser.add_argument('--metric', '-m', type=str, choices=['fid', 'is', 'mifid', 'kid'], default='fid')
     parser.add_argument('--gan', type=str,
                         choices=['DCGAN', 'LSGAN', 'WGAN-GP', 'HingeGAN', 'RSGAN', 'RaSGAN', 'RaLSGAN', 'RaHingeGAN'],
                         default='DCGAN')
